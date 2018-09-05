34dd6e10538780aa426935212e23de818b107b05
Andrew Kieser <a.kieser@yahoo.com>
2018-09-05 12:40:16 -0500

    Inital Updates
    Completeion of file creation.

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Playlist.css';
+
+class Playlist extends React.Component {
+	render() {
+		<div class="Playlist">
+			<input value="New Playlist"/>
+			<!-- Add a TrackList component -->
+			<a class="Playlist-save">SAVE TO SPOTIFY</a>
+		</div>
+	}
+}
+
+export default Playlist;
