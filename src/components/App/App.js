e2bff80afd2ab051787df98c8949c5cef283a830
Andrew Kieser <a.kieser@yahoo.com>
2018-09-11 12:46:50 -0500

    Cannot map undefined

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -52,9 +52,7 @@ class App extends React.Component {
 
 	search(term){
 		Spotify.search(term).then(searchResults => {
-			this.setState( {
-				searchResults: searchResults
-			});
+			this.setState({searchResults: searchResults});
 		});
 	}
 
@@ -63,7 +61,7 @@ class App extends React.Component {
 			<div>
 				<h1>Ja<span className="highlight">mmm</span>ing</h1>
 				<div className="App">
-					<SearchBar onSearch={this.search} />
+					<SearchBar search={this.search} />
 					<div className="App-playlist">
 						<SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
 						<Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
