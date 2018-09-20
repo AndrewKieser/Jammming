dcf4454d05a45d59722b234892fbe59b3be623f2
Andrew Kieser <a.kieser@yahoo.com>
2018-09-20 12:39:53 -0500

    View in Spot button

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -15,16 +15,17 @@ class Playlist extends React.Component {
 
 	render() {
 		return (
-			<div className="Playlist">
-				<input
-					onChange={this.handleNameChange}
-					defaultValue="New Playlist"/>
-				<Tracklist
-					tracks={this.props.playlistTracks}
-					onRemove={this.props.onRemove}
-					isRemoval={true} />
-				<a className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</a>
-			</div>
+				<div className="Playlist">
+					<input
+						onChange={this.handleNameChange}
+						defaultValue="New Playlist"/>
+					<Tracklist
+						tracks={this.props.playlistTracks}
+						onRemove={this.props.onRemove}
+						isRemoval={true} />
+					<a className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</a>
+					<a className="Playlist-save" href='https://open.spotify.com/collection/playlists' >VIEW IN SPOTIFY</a>
+				</div>
 		)
 	}
 }
